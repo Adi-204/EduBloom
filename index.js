daa07b3a4101db8462ba13ea09daa9158f6eb90f
Adi-204 <adiboghawala@gmail.com>
2023-10-26 18:23:17 +0530

    Initial Setup

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import ejs from 'ejs';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import session from 'express-session';
+import passportLocalMongoose from 'passport-local-mongoose';
+import 'dotenv/config';
+
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/home',(req,res)=>{
+    res.render('home');
+});
+
+app.get('/courses',(req,res)=>{
+    res.render('courses');
+});
+
+app.get('/signin',(req,res)=>{
+    res.render('signin');
+});
+
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+});
+
+app.get('/sessions',(req,res)=>{
+    res.render('sessions');
+});
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+});
+
